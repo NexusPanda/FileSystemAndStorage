45f8f4caa829183f6707355b616273406e22b827
Panda <157189888+NexusPanda@users.noreply.github.com>
2025-07-09 09:25:50 +0530

    Add files via upload

---
+++ FileManagementAndStorage/FileManagementAndStorage/src/main/java/com/example/FileManagementAndStorage/FileManagementAndStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileManagementAndStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementAndStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementAndStorageApplication.class, args);
+	}
+
+}
---
+++ FileManagementAndStorage/FileManagementAndStorage/src/test/java/com/example/FileManagementAndStorage/FileManagementAndStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileManagementAndStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementAndStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
