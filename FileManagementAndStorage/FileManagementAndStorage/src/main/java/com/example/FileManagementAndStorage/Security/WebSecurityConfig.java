6138d0bb016581a436a8bc1e5a2dbce22379d405
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-09-13 23:40:09 +0530

    Enabled Spring Security and added the basic Web Security Filter.

---
+++ FileManagementAndStorage/FileManagementAndStorage/src/main/java/com/example/FileManagementAndStorage/Security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.FileManagementAndStorage.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(header ->
+                header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
