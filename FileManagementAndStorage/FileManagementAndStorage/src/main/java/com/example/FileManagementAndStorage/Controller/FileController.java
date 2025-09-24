097e58b90209b67974a989c0a59b11c1686551b3
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-09-24 10:15:08 +0530

    In File controller, have updated the Username String as Principle and access the username from JWT and added Role based Authentication.

--- FileManagementAndStorage/FileManagementAndStorage/src/main/java/com/example/FileManagementAndStorage/Controller/FileController.java
+++ FileManagementAndStorage/FileManagementAndStorage/src/main/java/com/example/FileManagementAndStorage/Controller/FileController.java
@@ -4,6 +4,7 @@ import com.example.FileManagementAndStorage.ModelDTO.FileDTO;
 import com.example.FileManagementAndStorage.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,32 +18,37 @@ public class FileController {
     private FileService fileService;
 
     @PostMapping("/upload")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<?> uploadFile(
             @RequestParam("file") MultipartFile multipartFile,
             @RequestParam(value = "folderId", required = false) Long folderId,
-            @RequestParam("username") String username) { // temporary, later take from JWT
+            Principal principal) { // temporary, later take from JWT
 
-        return ResponseEntity.ok(fileService.uploadFile(multipartFile, folderId, username));
+        return ResponseEntity.ok(fileService.uploadFile(multipartFile, folderId, principal.getName()));
     }
 
     @GetMapping("/{id}/download")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<byte[]> getFile(@PathVariable Long id) {
         return fileService.downloadFile(id);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<String> softDelete(@PathVariable Long id) {
         fileService.softDelete(id);
         return ResponseEntity.ok("File moved to trash");
     }
 
     @PatchMapping("/{id}/restore")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<String> restore(@PathVariable Long id) {
         fileService.restoreFile(id);
         return ResponseEntity.ok("File restored");
     }
 
     @DeleteMapping("/{id}/permanent")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<Void> permanentDelete(@PathVariable Long id) {
         fileService.permanentDelete(id);
         return ResponseEntity.noContent().build();
